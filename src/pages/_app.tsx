6317ecc22ae8b98ab3294dd5c72dbc557838648f
Matthew Bernet <mattbernetGT@gmail.com>
2021-06-24 11:18:45 -0400

    Add Chakra and adjust project structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import "src/styles/globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ChakraProvider>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ChakraProvider>
   );
 }
 export default MyApp;
