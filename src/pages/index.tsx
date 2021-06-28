53ebb622a96db535652cdf36cde7cdc2e5bdae70
lyann1999 <70612133+lyann1999@users.noreply.github.com>
2021-06-27 23:36:56 -0400

    trial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Box, Heading } from "@chakra-ui/react";
 
 function LandingPage() {
   return (
-    <Flex height="100%" justifyContent="center" alignItems="center">
-      <Text fontSize="2xl">Landing Page.</Text>
-    </Flex>
+    <Box>
+      <Flex height="100%" justifyContent="center" alignItems="center">
+        <Box bg="tomato" w="100%" p={20}>
+          <Heading>Non-profit Portal</Heading>
+          <Text fontSize="2xl">
+            We build software, websites, and mobile apps to turn your ideas into
+            real products. If you have an existing project or a team, we are
+            happy to get involved in your development cycle.
+          </Text>
+        </Box>
+        <Text fontSize="2xl">Landing Page.</Text>
+      </Flex>
+    </Box>
   );
 }
 
