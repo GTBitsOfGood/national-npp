7e9f2517bb84b8b58ade7ec347edd17ff1196ba8
Matthew Bernet <mattbernetGT@gmail.com>
2021-06-27 22:25:00 -0400

    Add main landing page images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { Flex, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import FirstLandingImage from "public/images/landing_first.svg";
+import SecondLandingImage from "public/images/landing_second.svg";
+import ThirdLandingImage from "public/images/landing_third.svg";
 
 function LandingPage() {
   return (
-    <Flex height="100%" justifyContent="center" alignItems="center">
+    <Flex height="100%" justifyContent="space-around" alignItems="center">
       <Text fontSize="2xl">Landing Page.</Text>
+      <Image
+        src={FirstLandingImage}
+        alt="Build Websites"
+        width={500}
+        height={500}
+      />
     </Flex>
   );
 }
