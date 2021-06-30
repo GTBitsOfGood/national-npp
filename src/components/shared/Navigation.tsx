d13356a247635d47e5e7db06bbe551dd6dfbc587
Marlene Garcia <marlenegarcia20@gmail.com>
2021-06-30 08:46:20 -0400

    fixed according to comments

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-multi-comp */
 import {
   Flex,
   Text,
@@ -11,10 +10,6 @@ import {
   MenuList,
   MenuItem,
   Link,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
 import Image from "next/image";
@@ -24,39 +19,59 @@ import Logo from "public/images/small_logo.svg";
 function Navigation() {
   // Assume that you know this information, nav bar changes based on this information
   const isLoggedIn = true;
-  const isChapter = false;
-  const isNonprofit = true;
+  const isChapter = true;
+  const isNonprofit = false;
   const npUserName = "liv.2b.girl";
   const nonProftName = "Liv2BGirl";
   const chapterName = "Chapter Name";
 
-  //colors
-  const black = "rgba(51, 51, 51, 1)";
-  const slate = "rgba(101, 119, 136, 1)";
-  const blue = "rgba(0, 105, 202, 1)";
+  let NAV_ITEMS: Array<NavItem> = [];
+  if (isLoggedIn) {
+    NAV_ITEMS = isChapter ? NAV_ITEMS_ADMIN : NAV_ITEMS_NON_PROFIT;
+  }
 
   return (
     <Box>
       <Flex
-        bg={useColorModeValue("white", "gray.800")}
-        color={useColorModeValue("gray.600", "white")}
+        bg={"white"}
+        color={"gray.600"}
         maxH={"66px"}
         py="23px"
         px="34px"
         borderBottom={1}
         borderStyle={"solid"}
-        borderColor={useColorModeValue("gray.200", "gray.900")}
+        borderColor={"gray.200"}
         align={"center"}
       >
-        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+        <Flex flex="1" justify="start">
           <Image src={Logo} width={150} height={150} />
           {isLoggedIn && (isChapter || isNonprofit) ? (
             <Flex display={{ base: "none", md: "flex" }} ml={6}>
-              <DesktopNav
-                isLoggedIn={isLoggedIn}
-                isChapter={isChapter}
-                isNonprofit={isNonprofit}
-              />
+              <Stack direction={"row"} spacing={6} align="center">
+                {NAV_ITEMS.map((navItem) => (
+                  <Box key={navItem.label} placement={"bottom-start"}>
+                    <NextLink href={navItem.href ?? "/"}>
+                      <Link
+                        p={2}
+                        href={navItem.href ?? "/"}
+                        fontSize={"sm"}
+                        fontWeight={500}
+                        color={"slategrey"}
+                        _hover={{
+                          textDecoration: "none",
+                          color: "black",
+                        }}
+                        _active={{
+                          textDecoration: "none",
+                          color: "blue.500",
+                        }}
+                      >
+                        {navItem.label}
+                      </Link>
+                    </NextLink>
+                  </Box>
+                ))}
+              </Stack>
             </Flex>
           ) : null}
         </Flex>
@@ -64,10 +79,10 @@ function Navigation() {
           <Flex alignItems={"center"}>
             <Menu>
               <VStack spacing={-2} align="flex-end">
-                <Text p={1} fontSize="sm" fontWeight={700} color={black}>
+                <Text p={1} fontSize="sm" fontWeight={700} color={"black.400"}>
                   {isChapter ? chapterName : nonProftName}
                 </Text>
-                <Text p={1} fontSize="sm" fontWeight={500} color={slate}>
+                <Text p={1} fontSize="sm" fontWeight={500} color={"slategrey"}>
                   {isChapter ? "Admin" : npUserName}
                 </Text>
               </VStack>
@@ -77,10 +92,7 @@ function Navigation() {
                 variant={"link"}
                 cursor={"pointer"}
               >
-                <Avatar
-                  size={"sm"}
-                  src={"https://hack4impact.org/brokenLink"}
-                />
+                <Avatar size={"sm"} />
               </MenuButton>
               <MenuList>
                 <MenuItem>Edit Profile</MenuItem>
@@ -100,10 +112,10 @@ function Navigation() {
               fontSize={"sm"}
               fontWeight={600}
               color={"white"}
-              bg={blue}
+              bg={"blue.500"}
               href={"#"}
               _hover={{
-                bg: "rgba(0, 105, 202, 0.5)",
+                bg: "blue.400",
               }}
             >
               Log In
@@ -117,110 +129,9 @@ function Navigation() {
 
 export default Navigation;
 
-const DesktopNav = (props: {
-  isLoggedIn: boolean;
-  isChapter: boolean;
-  isNonprofit: boolean;
-}) => {
-  // const linkColor = useColorModeValue("gray.600", "gray.200");
-  // const linkHoverColor = useColorModeValue("gray.800", "white");
-  // const popoverContentBgColor = useColorModeValue("white", "gray.800");
-
-  //colors
-  const slate = "rgba(101, 119, 136, 1)";
-  const black = "rgba(51, 51, 51, 1)";
-  const blue = "rgba(0, 105, 202, 1)";
-
-  let NAV_ITEMS: Array<NavItem> = [];
-  if (props.isLoggedIn) {
-    if (props.isChapter) {
-      NAV_ITEMS = NAV_ITEMS_ADMIN;
-    } else {
-      NAV_ITEMS = NAV_ITEMS_NON_PROFIT;
-    }
-  }
-  return (
-    <Stack direction={"row"} spacing={6} align="center">
-      {NAV_ITEMS.map((navItem) => (
-        <Box key={navItem.label}>
-          <Popover trigger={"hover"} placement={"bottom-start"}>
-            <PopoverTrigger>
-              <NextLink href={navItem.href ?? "/"}>
-                <Link
-                  p={2}
-                  href={navItem.href ?? "/"}
-                  fontSize={"sm"}
-                  fontWeight={500}
-                  color={slate}
-                  _hover={{
-                    textDecoration: "none",
-                    color: black,
-                  }}
-                  _active={{
-                    fontWeight: 600,
-                    textDecoration: "none",
-                    color: blue,
-                  }}
-                >
-                  {navItem.label}
-                </Link>
-              </NextLink>
-            </PopoverTrigger>
-
-            {/* {navItem.children && ( // not needed rn, maybe later???
-              <PopoverContent
-                border={0}
-                boxShadow={"xl"}
-                bg={popoverContentBgColor}
-                p={4}
-                rounded={"xl"}
-                minW={"sm"}
-              >
-                <Stack>
-                  {navItem.children.map((child) => (
-                    <DesktopSubNav key={child.label} {...child} />
-                  ))}
-                </Stack>
-              </PopoverContent>
-            )} */}
-          </Popover>
-        </Box>
-      ))}
-    </Stack>
-  );
-};
-
-// not sure if we will need this in the future ?
-// const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
-//   return (
-//     <Link
-//       href={href}
-//       role={"group"}
-//       display={"block"}
-//       p={2}
-//       rounded={"md"}
-//       _hover={{ bg: useColorModeValue("pink.50", "gray.900") }}
-//     >
-//       <Stack direction={"row"} align={"center"}>
-//         <Box>
-//           <Text
-//             transition={"all .3s ease"}
-//             _groupHover={{ color: "pink.400" }}
-//             fontWeight={500}
-//           >
-//             {label}
-//           </Text>
-//           <Text fontSize={"sm"}>{subLabel}</Text>
-//         </Box>
-//       </Stack>
-//     </Link>
-//   );
-// };
-
 interface NavItem {
   label: string;
   subLabel?: string;
-  children?: Array<NavItem>;
   href?: string;
 }
 
