d670ad6f2de6e6d1286b87ff850dbeb08f23f4bc
Max Karpawich <maxkarpawich@gmail.com>
2021-07-02 12:53:12 -0400

    feat: improve Chapter Projects page flexibility

--- src/components/chapter/projects/ChapterProjectsTable.tsx
+++ src/components/chapter/projects/ChapterProjectsTable.tsx
@@ -15,33 +15,56 @@ function ChapterProjectsTable(props: { projects: ChapterProject[] }) {
 
   return (
     <Table variant="unstyled" size="lg">
-      <Thead color="#999999">
-        <Tr>
-          <Th>Organization</Th>
-          <Th>Website</Th>
-          <Th>E-mail</Th>
-          <Th>Start Date</Th>
-          <Th>Last Updated</Th>
-          <Th>Status</Th>
+      <Thead color="#999999" borderBottom="1px solid #E2E8F0">
+        <Tr height="100px">
+          <Th paddingInlineStart={5} paddingInlineEnd={4}>
+            Organization
+          </Th>
+          <Th paddingInlineStart={4} paddingInlineEnd={4}>
+            Website
+          </Th>
+          <Th paddingInlineStart={4} paddingInlineEnd={4}>
+            E-mail
+          </Th>
+          <Th paddingInlineStart={4} paddingInlineEnd={4}>
+            Start Date
+          </Th>
+          <Th paddingInlineStart={4} paddingInlineEnd={4}>
+            Last Updated
+          </Th>
+          <Th paddingInlineStart={4} paddingInlineEnd={4}>
+            Status
+          </Th>
         </Tr>
       </Thead>
-      <Tbody color="#333333">
+      <Tbody color="#333333" overflowY="auto">
         {projects.map((project) => (
-          <Tr key={project.id}>
-            <Td fontWeight={600}>{project.org}</Td>
-            <Td>
+          <Tr
+            key={project.id}
+            height="100px"
+            cursor="pointer"
+            _hover={{ backgroundColor: "rgba(0, 105, 202, 0.05)" }}
+          >
+            <Td paddingInlineStart={5} paddingInlineEnd={4} fontWeight={600}>
+              {project.org}
+            </Td>
+            <Td paddingInlineStart={4} paddingInlineEnd={4}>
               <Link href={`https://${project.website}`} isExternal>
                 {project.website}
               </Link>
             </Td>
-            <Td>
+            <Td paddingInlineStart={4} paddingInlineEnd={4}>
               <Link href={`mailto:${project.email}`} isExternal>
                 {project.email}
               </Link>
             </Td>
-            <Td>{dateToString(project.started)}</Td>
-            <Td>{dateToString(project.lastUpdated)}</Td>
-            <Td>
+            <Td paddingInlineStart={4} paddingInlineEnd={4}>
+              {dateToString(project.started)}
+            </Td>
+            <Td paddingInlineStart={4} paddingInlineEnd={4}>
+              {dateToString(project.lastUpdated)}
+            </Td>
+            <Td paddingInlineStart={4} paddingInlineEnd={4}>
               <ChapterProjectStatus status={project.status} />
             </Td>
           </Tr>
--- src/pages/chapter/projects/index.tsx
+++ src/pages/chapter/projects/index.tsx
@@ -9,8 +9,10 @@ import {
 import ChapterProjectsTable from "src/components/chapter/projects/ChapterProjectsTable";
 
 function ChapterProjects() {
-  const projects = [
-    {
+  const projects = [];
+
+  for (let i = 0; i < 10; i += 1) {
+    projects.push({
       id: "123",
       org: "Bits of Good",
       website: "bitsofgood.org",
@@ -18,17 +20,18 @@ function ChapterProjects() {
       started: new Date(),
       lastUpdated: new Date(),
       status: "newApp",
-    },
-    {
-      id: "456",
-      org: "Hack4Impact",
-      website: "hack4impact.org",
-      email: "hello@hack4impact.org",
-      started: new Date(),
-      lastUpdated: new Date(),
-      status: "rejected",
-    },
-  ];
+    });
+  }
+
+  projects.push({
+    id: "456",
+    org: "Hack4Impact",
+    website: "hack4impact.org",
+    email: "hello@hack4impact.org",
+    started: new Date(),
+    lastUpdated: new Date(),
+    status: "rejected",
+  });
 
   const closedFilter = (status: string) =>
     status === "delivered" || status === "rejected" || status === "cancelled";
@@ -39,22 +42,23 @@ function ChapterProjects() {
   return (
     <Flex
       height="100%"
-      justifyContent="center"
+      justifyContent="flex-start"
       alignItems="stretch"
       backgroundColor="#EBEEF1"
+      overflow="auto"
     >
       <Flex
-        justifyContent="center"
-        alignItems="stretch"
-        flexDirection="column"
         margin={50}
         padding={50}
-        flex={1}
         border="1px solid #657788"
         borderRadius={10}
         backgroundColor="white"
+        flexShrink={0}
+        flexGrow={1}
+        justifyContent="center"
+        alignItems="stretch"
       >
-        <Tabs height="100%" display="flex" flexDirection="column">
+        <Tabs flexGrow={1} display="flex" flexDirection="column">
           <TabList flex="0 0" borderBottom="none">
             <Tab
               width={120}
@@ -100,12 +104,14 @@ function ChapterProjects() {
             borderBottomLeftRadius={10}
             borderTopRightRadius={10}
             borderBottomRightRadius={10}
-            flex="1 1"
+            flex="1 1 auto"
+            overflowX="hidden"
+            overflowY="auto"
           >
-            <TabPanel>
+            <TabPanel paddingTop={0}>
               <ChapterProjectsTable projects={activeProjects} />
             </TabPanel>
-            <TabPanel>
+            <TabPanel paddingTop={0}>
               <ChapterProjectsTable projects={closedProjects} />
             </TabPanel>
           </TabPanels>
