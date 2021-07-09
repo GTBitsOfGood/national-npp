295a0a27077b3099946954dcaf6278e28115c3b5
lyann1999 <70612133+lyann1999@users.noreply.github.com>
2021-07-08 20:56:45 -0400

    added card component

---
+++ src/components/ChapterCard.tsx
@@ -0,0 +1,106 @@
+import { Box, Heading, Text, Link, HStack, Icon } from "@chakra-ui/react";
+//import React, { useState } from "react";
+import { AiFillFacebook } from "react-icons/ai";
+import { FiInstagram } from "react-icons/fi";
+import { IoIosGlobe } from "react-icons/io";
+
+const cardItems = [
+  {
+    name: "University of Washington",
+    email: "example1@gmail.com",
+    projectTypes: ["Website", "Mobile"],
+    website: "website1.com",
+    facebook: "whatever",
+    address: "Washington 1",
+  },
+  {
+    name: "University of Washington 2",
+    email: "example2@gmail.com",
+    projectTypes: ["Website"],
+    website: "website2.com",
+    facebook: "whatever",
+    instagram: "hello",
+    address: "Washington 2",
+  },
+  {
+    name: "University of Washington3",
+    email: "example3@gmail.com",
+    projectTypes: ["Website", "Mobile"],
+    facebook: "whatever",
+    website: "website3.com",
+    instagram: "hello",
+    address: "Washington 3",
+  },
+  {
+    name: "University of Washington4",
+    email: "example4@gmail.com",
+    projectTypes: ["Website", "Mobile", "Mobile 2"],
+    facebook: "whatever",
+    website: "website4.com",
+    address: "Washington 4",
+  },
+  {
+    name: "University of Washington5",
+    email: "example5@gmail.com",
+    projectTypes: ["Website", "Mobile", "Mobile 2"],
+    facebook: "whatever",
+    website: "website5.com",
+    address: "Washington 5",
+    instagram: "@hello",
+  },
+];
+
+const CircleIcon = (props) => (
+  <Icon viewBox="0 0 200 200" {...props}>
+    <path
+      fill="currentColor"
+      d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+    />
+  </Icon>
+);
+
+function ChapterCard() {
+  // const [isSelected, setSelected] = useState(false);
+
+  // const selected = () => setSelected(true);
+
+  return cardItems.map((cardItem) => (
+    <Box
+      key={cardItem.name}
+      w={60}
+      h={40}
+      border="1px"
+      borderRadius="15px"
+      borderColor="blackAlpha.600"
+      margin="4"
+      _hover={{ backgroundColor: "rgb(240, 248, 255)" }}
+      backgroundColor="white"
+      // onClick={selected}
+    >
+      <Box marginLeft="4" marginTop="5">
+        <Heading fontSize="medium" color="black" marginBottom="2">
+          {cardItem.name}
+        </Heading>
+        <HStack spacing="0.5">
+          <Text fontSize="small" color="gray.500">
+            {cardItem.address}
+          </Text>
+          <CircleIcon boxSize={1} color="blue.600" />
+          <Link fontSize="small" color="blue.600" href={cardItem.website}>
+            Contact
+          </Link>
+        </HStack>
+        <Text fontSize="small" color="gray.500">
+          {cardItem.projectTypes.join(", ")}
+        </Text>
+        <HStack />
+        <HStack spacing="2" marginTop="10">
+          {cardItem.email ? <IoIosGlobe /> : ""}
+          {cardItem.instagram ? <FiInstagram /> : ""}
+          {cardItem.facebook ? <AiFillFacebook /> : ""}
+        </HStack>
+      </Box>
+    </Box>
+  ));
+}
+export default ChapterCard;
--- src/pages/chapters.tsx
+++ src/pages/chapters.tsx
@@ -1,7 +1,47 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading, Button, Box } from "@chakra-ui/react";
+import React from "react";
+import ChapterCard from "src/components/ChapterCard";
 
 function ChapterSelectPage() {
-  return <Flex>Page for a nonprofit to select a chapter to work with</Flex>;
-}
+  // const [cardItems, setCardItems] = React.useState([]);
 
+  // React.useEffect(() => {
+  // 	fetch('')
+  // 	.then(response => response.json())
+  //   .then(data => setCardItems(data))
+  //   .catch(error => {
+  //     console.error(error)
+  //   })
+  //   }, [])
+
+  return (
+    <Flex
+      p={{ base: 5, md: 10 }}
+      direction="column"
+      alignItems="space-between"
+      justifyContent="center"
+      backgroundColor="gray.100"
+      height="100%"
+    >
+      <Box align="left">
+        <Heading fontSize={{ base: "medium", md: "x-large" }} marginBottom="5">
+          Select a chapter to work with
+        </Heading>
+      </Box>
+
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        spacing="2"
+        marginBottom="5"
+        flexWrap="wrap"
+      >
+        <ChapterCard />
+      </Flex>
+      <Box align="right">
+        <Button colorScheme="blue">Next</Button>
+      </Box>
+    </Flex>
+  );
+}
 export default ChapterSelectPage;
