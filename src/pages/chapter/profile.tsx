97b1117213f569802b0affc74b6258b597ffeca1
Marlene Garcia <marlenegarcia20@gmail.com>
2021-07-14 01:54:02 -0400

    made ui changes and added most functionality

--- src/pages/chapter/profile.tsx
+++ src/pages/chapter/profile.tsx
@@ -1,41 +1,60 @@
 /* eslint-disable react/forbid-elements */
 import {
   Flex,
-  Box,
   Text,
   VStack,
   HStack,
   Avatar,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
   Input,
   Spacer,
   Checkbox,
   CheckboxGroup,
   Select,
+  Button,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { states, countries } from "src/utils/constants";
 
 function ChapterProfilePage() {
+  //admin info
+  const [nameAdmin, setNameAdmin] = useState<string>("");
+  const [calendly, setCalendly] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+
+  //chapter info
   const [name, setName] = useState<string>("");
-  const [location, setLocation] = useState<string>("");
+  const [street, setStreet] = useState<string>("");
+  const [city, setCity] = useState<string>("");
+  const [state, setState] = useState<string>("");
+  const [zip, setZip] = useState<string>("");
+  const [country, setCountry] = useState<string>("");
+
   const [contact, setContact] = useState<string>("");
   const [projectLimit, setProjectLimit] = useState<number>(NaN);
   const [website, setWebsite] = useState<string>("");
   const [facebook, setFacebook] = useState<string>("");
   const [instagram, setInstagram] = useState<string>("");
-  const [chapterProjects, setChapterProjects] = useState<string[]>([]);
+  const [chapterProjects, setChapterProjects] = useState<boolean[]>([
+    false,
+    false,
+    false,
+  ]);
   const [projectProcessCustomization, setprojectProcessCustomization] =
-    useState<string[]>([
-      "In-Progress [Required]",
-      "Completed [Required]",
-      "Maintenance [Required]",
-    ]);
+    useState<boolean[]>([false, false, true, true, false, true]);
 
-  const [profileUpdate, setProfileUpdate] = useState<boolean>(false);
+  // const stateOptions = states.map((state, idx) => {
+  //   <option key={idx} value={state}>
+  //     {state}
+  //   </option>;
+  // });
 
+  // const countryOptions = countries.map((country, idx) => {
+  //   <option key={idx} value={country}>
+  //     {country}
+  //   </option>;
+  // });
   return (
     <Flex
       height="100%"
@@ -55,15 +74,68 @@ function ChapterProfilePage() {
           {"Profile"}
         </Text>
         <Flex
-          minW={800}
-          minH={950}
+          minW={850}
+          minH={1300}
           border="1px solid #657788"
           borderRadius={10}
           backgroundColor="white"
           justifyContent="center"
         >
-          <VStack margin={50} w={650}>
+          <VStack margin={50} w={680}>
             <Avatar width="80px" height="80px" marginBottom={50} />
+            <Text
+              alignSelf="flex-start"
+              fontSize="md"
+              fontWeight={700}
+              color={"black.400"}
+            >
+              {"User Information"}
+            </Text>
+            <Flex flexDirection="row" w={680} h={220}>
+              <Flex
+                flexDirection="column"
+                justifyContent="space-between"
+                marginBottom={10}
+              >
+                <FormControl id="name">
+                  <FormLabel fontSize="sm">Name</FormLabel>
+                  <Input
+                    type="name"
+                    fontSize="sm"
+                    width={320}
+                    placeholder="Full Name"
+                    value={nameAdmin}
+                    onChangeText={setNameAdmin}
+                    marginBottom={6}
+                  />
+                </FormControl>
+                <FormControl id="url">
+                  <FormLabel fontSize="sm">Calendly Link</FormLabel>
+                  <Input
+                    type="email"
+                    fontSize="sm"
+                    width={320}
+                    placeholder="calendly.com/"
+                    value={calendly}
+                    onChangeText={setCalendly}
+                  />
+                </FormControl>
+              </Flex>
+              <Spacer />
+              <Flex flexDirection="column" justifyContent="space-between">
+                <FormControl id="phone number">
+                  <FormLabel fontSize="sm">Phone Number (Optional)</FormLabel>
+                  <Input
+                    type="phone number"
+                    fontSize="sm"
+                    width={320}
+                    placeholder="(XXX) XXX-XXXX"
+                    value={phoneNumber}
+                    onChangeText={setPhoneNumber}
+                  />
+                </FormControl>
+              </Flex>
+            </Flex>
             <Text
               alignSelf="flex-start"
               fontSize="md"
@@ -72,17 +144,18 @@ function ChapterProfilePage() {
             >
               {"Chapter Information"}
             </Text>
-            <Flex flexDirection="row" w={640} h={370}>
+            <Flex flexDirection="row" w={680} h={440}>
               <Flex flexDirection="column" justifyContent="space-between">
                 <FormControl id="name">
                   <FormLabel fontSize="sm">Chapter Name</FormLabel>
                   <Input
                     type="name"
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="Chapter Name"
                     value={name}
                     onChangeText={setName}
+                    marginBottom={6}
                   />
                 </FormControl>
                 <FormControl id="email">
@@ -90,10 +163,11 @@ function ChapterProfilePage() {
                   <Input
                     type="email"
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="chapter@email.com"
                     value={contact}
                     onChangeText={setContact}
+                    marginBottom={6}
                   />
                 </FormControl>
                 <FormControl id="location">
@@ -101,46 +175,44 @@ function ChapterProfilePage() {
                   <Input
                     type="street"
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="Street"
-                    value={location}
-                    onChangeText={setLocation}
+                    value={street}
+                    onChangeText={setStreet}
+                    marginBottom={4}
                   />
                   <Input
                     type="city"
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="City"
-                    value={location}
-                    onChangeText={setLocation}
+                    value={city}
+                    onChangeText={setCity}
+                    marginBottom={4}
                   />
-                  <HStack>
+                  <HStack marginBottom={4}>
                     <Select
                       fontSize="sm"
                       width={191}
                       placeholder="Select State"
                     >
-                      <option value="option1">Option 1</option>
-                      <option value="option2">Option 2</option>
-                      <option value="option3">Option 3</option>
+                      {/* {stateOptions} */}
                     </Select>
                     <Input
                       type="zip code"
                       fontSize="sm"
-                      width={100}
+                      width={120}
                       placeholder="ZIP"
-                      value={location}
-                      onChangeText={setLocation}
+                      value={zip}
+                      onChangeText={setZip}
                     />
                   </HStack>
                   <Select
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="Select Country"
                   >
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
+                    {/* {countryOptions} */}
                   </Select>
                 </FormControl>
               </Flex>
@@ -151,10 +223,11 @@ function ChapterProfilePage() {
                   <Input
                     type="url"
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="chapter.org"
                     value={website}
                     onChangeText={setWebsite}
+                    marginBottom={6}
                   />
                 </FormControl>
                 <FormControl id="url">
@@ -162,10 +235,11 @@ function ChapterProfilePage() {
                   <Input
                     type="url"
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="facebook.com/"
                     value={facebook}
                     onChangeText={setFacebook}
+                    marginBottom={6}
                   />
                 </FormControl>
                 <FormControl id="url">
@@ -173,10 +247,11 @@ function ChapterProfilePage() {
                   <Input
                     type="url"
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="instagram.com/"
                     value={instagram}
                     onChangeText={setInstagram}
+                    marginBottom={5}
                   />
                 </FormControl>
                 <FormControl id="number">
@@ -184,26 +259,67 @@ function ChapterProfilePage() {
                   <Input
                     type="number"
                     fontSize="sm"
-                    width={299}
+                    width={320}
                     placeholder="Enter a number"
                     value={projectLimit}
                     onChangeText={setProjectLimit}
+                    marginBottom={5}
                   />
                 </FormControl>
                 <FormControl>
                   <FormLabel fontSize="sm">Chapter Products</FormLabel>
                   <CheckboxGroup>
-                    <HStack marginTop={3}>
-                      <Checkbox size="md">Websites</Checkbox>
-                      <Checkbox size="md">Web Apps</Checkbox>
-                      <Checkbox size="md">Mobile Apps</Checkbox>
+                    <HStack w={320}>
+                      <Checkbox
+                        size="md"
+                        isChecked={chapterProjects[0]}
+                        onChange={(e) =>
+                          setChapterProjects([
+                            e.target.checked,
+                            chapterProjects[1],
+                            chapterProjects[2],
+                          ])
+                        }
+                      >
+                        Websites
+                      </Checkbox>
+                      <Checkbox
+                        size="md"
+                        isChecked={chapterProjects[1]}
+                        onChange={(e) =>
+                          setChapterProjects([
+                            chapterProjects[0],
+                            e.target.checked,
+                            chapterProjects[2],
+                          ])
+                        }
+                      >
+                        Web Apps
+                      </Checkbox>
+                      <Checkbox
+                        size="md"
+                        isChecked={chapterProjects[2]}
+                        onChange={(e) =>
+                          setChapterProjects([
+                            chapterProjects[0],
+                            chapterProjects[1],
+                            e.target.checked,
+                          ])
+                        }
+                      >
+                        Mobile Apps
+                      </Checkbox>
                     </HStack>
                   </CheckboxGroup>
                 </FormControl>
               </Flex>
             </Flex>
             <Spacer />
-            <Flex direction={"column"}>
+            <Spacer />
+            <Spacer />
+            <Spacer />
+            <Spacer />
+            <Flex flexDirection="column" w={680} h={440}>
               <Text
                 alignSelf="flex-start"
                 fontSize="md"
@@ -224,17 +340,90 @@ function ChapterProfilePage() {
               <FormControl>
                 <Flex alignSelf="flex-start">
                   <CheckboxGroup>
-                    <VStack>
-                      <Checkbox size="md">Application</Checkbox>
-                      <Checkbox size="md">Interview</Checkbox>
-                      <Checkbox size="md">In-Progress [Required]</Checkbox>
-                      <Checkbox size="md">Completed [Required]</Checkbox>
-                      <Checkbox size="md">Feedback</Checkbox>
-                      <Checkbox size="md">Maintenance [Required]</Checkbox>
-                    </VStack>
+                    <Flex flexDirection="column" paddingTop={6}>
+                      <Checkbox
+                        size="md"
+                        isChecked={projectProcessCustomization[0]}
+                        onChange={(e) =>
+                          setChapterProjects([
+                            e.target.checked,
+                            chapterProjects[1],
+                            chapterProjects[2],
+                            chapterProjects[3],
+                            chapterProjects[4],
+                            chapterProjects[5],
+                          ])
+                        }
+                      >
+                        Application
+                      </Checkbox>
+                      <Checkbox
+                        size="md"
+                        paddingTop={2}
+                        isChecked={projectProcessCustomization[1]}
+                        onChange={(e) =>
+                          setChapterProjects([
+                            chapterProjects[0],
+                            e.target.checked,
+                            chapterProjects[2],
+                            chapterProjects[3],
+                            chapterProjects[4],
+                            chapterProjects[5],
+                          ])
+                        }
+                      >
+                        Interview
+                      </Checkbox>
+                      <Checkbox
+                        size="md"
+                        paddingTop={2}
+                        isDisabled
+                        defaultIsChecked
+                      >
+                        In-Progress [Required]
+                      </Checkbox>
+                      <Checkbox
+                        size="md"
+                        paddingTop={2}
+                        isDisabled
+                        defaultIsChecked
+                      >
+                        Completed [Required]
+                      </Checkbox>
+                      <Checkbox
+                        size="md"
+                        paddingTop={2}
+                        isChecked={projectProcessCustomization[4]}
+                        onChange={(e) =>
+                          setChapterProjects([
+                            chapterProjects[0],
+                            chapterProjects[1],
+                            chapterProjects[2],
+                            chapterProjects[3],
+                            e.target.checked,
+                            chapterProjects[5],
+                          ])
+                        }
+                      >
+                        Feedback
+                      </Checkbox>
+                      <Checkbox
+                        size="md"
+                        paddingTop={2}
+                        isDisabled
+                        defaultIsChecked
+                      >
+                        Maintenance [Required]
+                      </Checkbox>
+                    </Flex>
                   </CheckboxGroup>
                 </Flex>
               </FormControl>
+            </Flex>
+            <Flex flexDirection="row" alignSelf="flex-end">
+              <Button size="md" colorScheme="blue">
+                Save Changes
+              </Button>
             </Flex>
           </VStack>
         </Flex>
