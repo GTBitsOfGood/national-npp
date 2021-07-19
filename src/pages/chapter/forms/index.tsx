f2baaf76d1911e7b851b956659679787aa89d1c4
Matthew Bernet <mattbernetGT@gmail.com>
2021-07-19 08:53:09 -0400

    Add pages for sprint 4

---
+++ src/pages/chapter/forms/index.tsx
@@ -0,0 +1,7 @@
+import { Flex } from "@chakra-ui/react";
+
+function ChapterFormsPage() {
+  return <Flex>Chapter Forms Page</Flex>;
+}
+
+export default ChapterFormsPage;
---
+++ src/pages/chapter/projects/[id].tsx
@@ -0,0 +1,7 @@
+import { Flex } from "@chakra-ui/react";
+
+function ChapterProjectPage() {
+  return <Flex>Chapter Project Page</Flex>;
+}
+
+export default ChapterProjectPage;
