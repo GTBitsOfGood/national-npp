2c3743e954ab1d03f6ae8bf2ee9c0ca9fca35601
Matthew Bernet <mattbernetGT@gmail.com>
2021-08-17 05:29:05 -0400

    Fix chapter forms page responsiveness

--- src/components/FormCard.tsx
+++ src/components/FormCard.tsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   Heading,
   Text,
   HStack,
@@ -13,65 +12,67 @@ import {
   ModalCloseButton,
   Button,
   useDisclosure,
+  Flex,
 } from "@chakra-ui/react";
 import { FiEdit2 } from "react-icons/fi";
 import { RiDeleteBin5Line } from "react-icons/ri";
+import { dateToMMDDYYYY } from "src/utils/dates";
+
 function FormCard(props: { name: string; updatedAt: Date }) {
   const { name, updatedAt } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  function dateToString(updatedAt: Date) {
-    const dateOfMonth = `${updatedAt.getDate().toString().slice(-2)}`;
-    const month = `${(updatedAt.getMonth() + 1).toString().slice(-2)}`;
-    const year = updatedAt.getFullYear();
-
-    return `${month}/${dateOfMonth}/${year}`;
-  }
-
   return (
-    <Box borderBottom="1px" borderColor="#EBEEF1">
-      <HStack padding="30px 50px 30px 50px" justifyContent="space-between">
-        <HStack spacing="50px">
-          <Box width="150px">
-            <Heading fontSize="medium">{name}</Heading>
-          </Box>
-          <Box>
-            <Text fontSize="12px" color="#808080">
-              Last Edited: {dateToString(updatedAt)}
-            </Text>
-          </Box>
+    <Flex
+      borderBottom="1px solid #EBEEF1"
+      p="40px 60px"
+      justify="space-between"
+    >
+      <HStack spacing="50px">
+        <Flex width="150px">
+          <Heading
+            fontSize="medium"
+            textOverflow="ellipsis"
+            whiteSpace="nowrap"
+            overflow="hidden"
+          >
+            {name}
+          </Heading>
+        </Flex>
+        <Text fontSize="xs" color="#808080">
+          Last Edited: {dateToMMDDYYYY(updatedAt)}
+        </Text>
+      </HStack>
+      <HStack fontSize="12px" color="#1974d2" spacing="50px">
+        <HStack>
+          <FiEdit2 size="1.5em" />
+          <Link>Edit Form</Link>
         </HStack>
-        <HStack fontSize="12px" color="#1974d2" spacing="50px">
-          <HStack>
-            <FiEdit2 size="1.5em" />
-            <Link>Edit Form</Link>
-          </HStack>
-          <HStack>
-            <RiDeleteBin5Line size="1.5em" />
-            <Link onClick={onOpen}>Delete Form</Link>
-            <Modal isOpen={isOpen} onClose={onClose} size="md" isCentered>
-              <ModalOverlay />
-              <ModalContent borderColor="#657788" border="1px">
-                <ModalHeader>Delete Form</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody fontSize="small">
-                  Please confirm that you would like to delete {name}.
-                </ModalBody>
+        <HStack>
+          <RiDeleteBin5Line size="1.5em" />
+          <Link onClick={onOpen}>Delete Form</Link>
+          <Modal isOpen={isOpen} onClose={onClose} size="md" isCentered>
+            <ModalOverlay />
+            <ModalContent borderColor="#657788" border="1px">
+              <ModalHeader>Delete Form</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody fontSize="small">
+                Please confirm that you would like to delete {name}.
+              </ModalBody>
 
-                <ModalFooter>
-                  <Button colorScheme="red" mr={2}>
-                    Delete
-                  </Button>
-                  <Button variant="ghost" color="#1974d2" onClick={onClose}>
-                    Cancel
-                  </Button>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
-          </HStack>
+              <ModalFooter>
+                <Button colorScheme="red" mr={2}>
+                  Delete
+                </Button>
+                <Button variant="ghost" color="#1974d2" onClick={onClose}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </HStack>
       </HStack>
-    </Box>
+    </Flex>
   );
 }
 
--- src/pages/chapter/forms/index.tsx
+++ src/pages/chapter/forms/index.tsx
@@ -1,90 +1,107 @@
-import { Heading, Box, Button, HStack, Text } from "@chakra-ui/react";
+import { Heading, Button, HStack, Text, Flex } from "@chakra-ui/react";
 import { GoPlus } from "react-icons/go";
 import FormCard from "src/components/FormCard";
 import { FormType } from "src/utils/types";
 
-function ChapterFormsPage() {
-  const forms = [
-    {
-      _id: `1`,
-      name: "Application Form 1",
-      type: FormType.APPLICATION,
-      updatedAt: new Date(),
-    },
-    {
-      _id: `2`,
-      name: "Application Form 2",
-      type: FormType.APPLICATION,
-      updatedAt: new Date(),
-    },
-    {
-      _id: `3`,
-      name: "Feedback Form 1",
-      type: FormType.FEEDBACK,
-      updatedAt: new Date(),
-    },
-    {
-      _id: `4`,
-      name: "Application Form 1",
-      type: FormType.APPLICATION,
-      updatedAt: new Date(),
-    },
-    {
-      _id: `5`,
-      name: "Application Form 2",
-      type: FormType.APPLICATION,
-      updatedAt: new Date(),
-    },
-    {
-      _id: `6`,
-      name: "Feedback Form 1",
-      type: FormType.FEEDBACK,
-      updatedAt: new Date(),
-    },
-  ];
-  return (
-    <Box height="100%" backgroundColor="#EBEEF1" overflow="auto">
-      <Box
-        display="table"
-        padding="40px"
-        justifyContent="center"
-        alignItems="center"
-        margin="auto"
-        align="center"
-      >
-        <Box width="1000px">
-          <Heading
-            fontSize={{ base: "x-large", md: "xx-large" }}
-            marginBottom="30px"
-            align="left"
-          >
-            My Forms
-          </Heading>
-        </Box>
+// TODO: Remove these when real data available
+const forms = [
+  {
+    _id: `1`,
+    name: "Application Form 1",
+    type: FormType.APPLICATION,
+    updatedAt: new Date(),
+  },
+  {
+    _id: `2`,
+    name: "Application Form 2",
+    type: FormType.APPLICATION,
+    updatedAt: new Date(),
+  },
+  {
+    _id: `3`,
+    name: "Feedback Form 1",
+    type: FormType.FEEDBACK,
+    updatedAt: new Date(),
+  },
+  {
+    _id: `4`,
+    name: "Application Form 1",
+    type: FormType.APPLICATION,
+    updatedAt: new Date(),
+  },
+  {
+    _id: `5`,
+    name: "Application Form 2",
+    type: FormType.APPLICATION,
+    updatedAt: new Date(),
+  },
+  {
+    _id: `6`,
+    name: "Feedback Form 1",
+    type: FormType.FEEDBACK,
+    updatedAt: new Date(),
+  },
+  {
+    _id: `7`,
+    name: "Feedback Form 2",
+    type: FormType.FEEDBACK,
+    updatedAt: new Date(),
+  },
+  {
+    _id: `8`,
+    name: "Feedback Form 3",
+    type: FormType.FEEDBACK,
+    updatedAt: new Date(),
+  },
+  {
+    _id: `9`,
+    name: "Feedback Form 4",
+    type: FormType.FEEDBACK,
+    updatedAt: new Date(),
+  },
+];
 
-        <Box
-          flexDirection="column"
-          padding="30px"
-          backgroundColor="white"
-          border="1px"
-          borderColor="#657788"
-          borderRadius="15px"
+function ChapterFormsPage() {
+  return (
+    <Flex height="100%" backgroundColor="#EBEEF1" overflow="auto">
+      <Flex
+        direction="column"
+        my="30px"
+        flex="0 1 1000px"
+        minW="800px"
+        minH="700px"
+        marginX="auto"
+      >
+        <Heading
+          fontSize={{ base: "x-large", md: "xx-large" }}
+          marginBottom="30px"
+          ml="10px"
         >
-          <Box align="right" marginBottom="30px">
+          Chapter Forms
+        </Heading>
+        <Flex
+          direction="column"
+          bgColor="white"
+          flex="1"
+          p="30px"
+          border="1px solid #657788"
+          borderRadius="15px"
+          overflow="hidden"
+        >
+          <Flex alignSelf="flex-end" mb="30px">
             <Button colorScheme="blue">
               <HStack>
                 <GoPlus color="white" />
                 <Text>Create New Form</Text>
               </HStack>
             </Button>
-          </Box>
-          <Box
-            flexDirection="column"
-            backgroundColor="white"
-            border="1px"
+          </Flex>
+          <Flex
+            direction="column"
+            flex="1"
+            border="1px solid #EBEEF1"
             borderRadius="15px"
-            borderColor="#EBEEF1"
-            overflow="scroll"
+            overflow="auto"
           >
             {forms.map((form) => (
               <FormCard
@@ -93,10 +110,10 @@ function ChapterFormsPage() {
                 updatedAt={form.updatedAt}
               />
             ))}
-          </Box>
-        </Box>
-      </Box>
-    </Box>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
 
