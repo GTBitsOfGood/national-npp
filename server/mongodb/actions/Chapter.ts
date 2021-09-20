478600f4f4ef9e6d76357b12bc05ca5d54f23e98
Matthew Bernet <mattbernetGT@gmail.com>
2021-09-20 18:57:06 -0400

    Backend deletions based on updated requirements

--- server/mongodb/actions/Chapter.ts
+++ server/mongodb/actions/Chapter.ts
@@ -3,14 +3,6 @@ import ChapterModel from "server/mongodb/models/Chapter";
 import dbConnect from "server/utils/dbConnect";
 import { ChapterUpdate } from "src/utils/types";
 
-export async function getChapters() {
-  await dbConnect();
-
-  const chapters = await ChapterModel.find();
-
-  return chapters;
-}
-
 export async function updateChapter(
   chapterId: Types.ObjectId,
   chapter: ChapterUpdate
