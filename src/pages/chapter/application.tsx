dae8a1f6ada10a5e2023136935ab73cf8eeeaff6
Matthew Bernet <mattbernetGT@gmail.com>
2021-10-07 17:44:55 -0400

    Add new sprint pages

---
+++ src/pages/chapter/application.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+
+function ChapterApplicationPage() {
+  return (
+    <Flex height="100%" width="100%">
+      <Flex margin="auto">View Application</Flex>
+    </Flex>
+  );
+}
+
+export default ChapterApplicationPage;
---
+++ src/pages/chapter/applications.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+
+function ChapterApplicationsPage() {
+  return (
+    <Flex height="100%" width="100%">
+      <Flex margin="auto">Chapter Applications Page</Flex>
+    </Flex>
+  );
+}
+
+export default ChapterApplicationsPage;
