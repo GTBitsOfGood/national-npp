1087e36b8d710a5f04f9d9e777b420b4672f1593
Saurav Ghosal <sghosal9@gatech.edu>
2021-10-16 18:18:19 -0400

    nonprofit applications pages setup

--- src/pages/chapter/applications.tsx
+++ src/pages/chapter/applications.tsx
@@ -1,9 +1,164 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Th,
+  Tbody,
+  Td,
+  Tr,
+  Link,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+import { ObjectID } from "bson";
+import { useEffect, useState } from "react";
+import {
+  User,
+  Nonprofit,
+  Project,
+  ProjectStage,
+  Role,
+  ProjectType,
+} from "src/utils/types";
 
 function ChapterApplicationsPage() {
+  const [applications, setApplications] = useState<Partial<Project>[]>([]);
+
+  useEffect(() => {
+    // fetch applications from database here
+    const newApps: Project[] = [];
+    for (let i = 0; i < 20; i += 1) {
+      newApps.push({
+        _id: new ObjectID("7B39BDDDC643564D877D7770"),
+        nonprofit: {
+          _id: new ObjectID("7B39BDDDC643564D877D7770"),
+          contact: new ObjectID("7B39BDDDC643564D877D7770"),
+          name: "Liv2BGirl",
+          address: {
+            city: "Sydney",
+            street: "42 Wallaby Way",
+            state: "New South Wales",
+            zipCode: "50679",
+            country: "Australia",
+          },
+          isVerified: false,
+        },
+        contact: {
+          _id: new ObjectID("7B39BDDDC643564D877D7770"),
+          id: "test",
+          email: "test@xyz.com",
+          emailVerified: new Date(),
+          name: "Saurav Ghosal",
+          roles: [Role.CHAPTER_MEMBER],
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        type: ProjectType.WEBSITE,
+        chapter: new ObjectID("7B39BDDDC643564D877D7770"),
+        name: "test project",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status: ProjectStage.SUBMIT_APPLICATION,
+      });
+    }
+    setApplications(newApps);
+  }, []);
   return (
-    <Flex height="100%" width="100%">
-      <Flex margin="auto">Chapter Applications Page</Flex>
+    <Flex
+      height="100%"
+      width="100%"
+      justifyContent="center"
+      alignItems="stretch"
+      overflow="auto"
+    >
+      <Flex
+        minH="600px"
+        maxW="1200px"
+        maxH="1000px"
+        margin={50}
+        padding={50}
+        border="1px solid #657788"
+        borderRadius={10}
+        backgroundColor="surface"
+        flexShrink={0}
+        flexGrow={1}
+        justifyContent="center"
+        alignItems="stretch"
+      >
+        <Table
+          variant="unstyled"
+          size="lg"
+          backgroundColor="surface"
+          border="1px solid #E2E8F0"
+          borderBottomLeftRadius={10}
+          borderTopRightRadius={10}
+          borderBottomRightRadius={10}
+          flex="1 1 auto"
+          overflowX="hidden"
+        >
+          <Thead color="#999999" borderBottom="1px solid #E2E8F0">
+            <Tr height="100px">
+              <Th paddingInlineStart={5} paddingInlineEnd={4}>
+                Organization
+              </Th>
+              <Th paddingInlineStart={4} paddingInlineEnd={4}>
+                Project Name
+              </Th>
+              <Th paddingInlineStart={4} paddingInlineEnd={4}>
+                Contact Email
+              </Th>
+              <Th paddingInlineStart={4} paddingInlineEnd={4}>
+                Project Type
+              </Th>
+              <Th paddingInlineStart={4} paddingInlineEnd={4}>
+                Status
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody overflowY="auto">
+            {applications &&
+              applications.length !== 0 &&
+              applications.map((application, index) => (
+                <Tr
+                  key={index}
+                  height="100px"
+                  cursor="pointer"
+                  _hover={{ backgroundColor: "rgba(0, 105, 202, 0.05)" }}
+                >
+                  {console.log(application.contact)}
+                  <Td
+                    paddingInlineStart={5}
+                    paddingInlineEnd={4}
+                    fontWeight={600}
+                  >
+                    {(application.nonprofit as Nonprofit).name}
+                  </Td>
+                  <Td paddingInlineStart={4} paddingInlineEnd={4}>
+                    {application.name}
+                  </Td>
+                  <Td paddingInlineStart={4} paddingInlineEnd={4}>
+                    <Link
+                      href={`mailto:${(application.contact as User).email}`}
+                      isExternal
+                    >
+                      {(application.contact as User).email}
+                    </Link>
+                  </Td>
+                  <Td paddingInlineStart={4} paddingInlineEnd={4}>
+                    {application.type}
+                  </Td>
+                  <Td paddingInlineStart={4} paddingInlineEnd={4}>
+                    <Button
+                      colorScheme="blue"
+                      onClick={() => console.log("redirecting to new page!")}
+                    >
+                      View Application
+                    </Button>
+                  </Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      </Flex>
     </Flex>
   );
 }
