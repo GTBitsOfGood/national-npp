bd32dd95eb1a18eb134cb3f89836b1eb305448cd
Saurav Ghosal <sghosal9@gatech.edu>
2021-11-11 16:47:19 -0500

    fetching issues from backend, calculating dates

--- src/pages/nonprofit/issues/index.tsx
+++ src/pages/nonprofit/issues/index.tsx
@@ -12,41 +12,78 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { nonprofitGetProjects } from "src/actions/Project";
+import { nonprofitGetIssues, nonprofitGetProjects } from "src/actions/Project";
 import IssuesTable from "src/components/nonprofit/issues/IssuesTable";
 import MaintenanceProjectCard from "src/components/nonprofit/issues/MaintenanceProjectCard";
+import { dateDiffInDays, dateToMMDDYYYY } from "src/utils/dates";
 import { showError } from "src/utils/notifications";
-import { Issue, Project, ProjectStage } from "src/utils/types";
+import {
+  Issue,
+  IssueStatus,
+  Chapter,
+  Project,
+  ProjectStage,
+} from "src/utils/types";
 import urls from "src/utils/urls";
 
 function NonprofitIssuesPage() {
   const [projects, setProjects] = useState<Project[]>();
   const [currProject, setCurrProject] = useState<Project>();
   const [issues, setIssues] = useState<Issue[]>();
-  const [open, setOpen] = useState(true);
 
   useEffect(() => {
     async function fetchProjects() {
-      const projects: Project[] = await nonprofitGetProjects({
+      let projects: Project[] = await nonprofitGetProjects({
         status: ProjectStage.MAINTENANCE,
       });
 
+      projects = projects.map((project) => {
+        // maintenance start will never be undefined if stage is maintenance
+        const endDate = new Date(project.maintenanceStart!);
+        endDate.setDate(
+          endDate.getDate() + (project.chapter as Chapter).maintenancePeriod
+        );
+        return {
+          ...project,
+          maintenanceEnd: endDate,
+          remainingDays: dateDiffInDays(new Date(), endDate),
+        };
+      });
       if (projects.length !== 0) {
         setCurrProject(projects[0]);
       }
-
       setProjects(projects);
     }
 
     fetchProjects().catch((error: Error) => showError(error.message));
   }, []);
 
+  useEffect(() => {
+    async function fetchIssues() {
+      if (currProject) {
+        const issues: Issue[] = await nonprofitGetIssues(
+          currProject._id.toString()
+        );
+        setIssues(issues);
+      }
+    }
+
+    fetchIssues().catch((error: Error) => showError(error.message));
+  }, [currProject]);
+
+  const findOpenIssues = (issue: Issue) =>
+    issue.status == IssueStatus.IN_PROGRESS ||
+    issue.status == IssueStatus.PENDING;
+
+  const findClosedIssues = (issue: Issue) =>
+    issue.status == IssueStatus.CLOSED || issue.status == IssueStatus.RESOLVED;
+
   return (
     <Flex height="100%" width="100%" overflow="auto">
       <Flex
         display={{ base: "none", lg: "flex" }}
         direction="column"
-        minHeight="700px"
+        alignSelf="stretch"
         height="100%"
         width="400px"
         minWidth="300px"
@@ -58,8 +95,12 @@ function NonprofitIssuesPage() {
             Projects Open for Maintenance
           </Heading>
           <VStack>
-            {projects?.map((project) => (
+            {projects?.map((project: Project) => (
               <MaintenanceProjectCard
+                remainingDays={dateDiffInDays(
+                  new Date(),
+                  project.maintenanceEnd!
+                )}
                 key={project.name}
                 isSelected={project._id === currProject?._id}
                 project={project}
@@ -92,10 +133,13 @@ function NonprofitIssuesPage() {
               paddingY="15px"
             >
               <Text fontWeight="bold">
-                Maintenance Period: 02/26/2021 - 05/07/2021
+                Maintenance Period:{" "}
+                {currProject &&
+                  dateToMMDDYYYY(new Date(currProject.maintenanceStart!))}{" "}
+                - {currProject && dateToMMDDYYYY(currProject.maintenanceEnd!)}
               </Text>
               <Link href={urls.pages.nonprofit.issues.create} passHref>
-                <Button variant="primary">File a Request</Button>
+                <Button variant="primary">File a Maintenance Request</Button>
               </Link>
             </Flex>
             <Tabs>
@@ -104,7 +148,6 @@ function NonprofitIssuesPage() {
                   _focus={{
                     boxShadow: "none",
                   }}
-                  onClick={() => setOpen(true)}
                 >
                   Open Issues
                 </Tab>
@@ -112,22 +155,21 @@ function NonprofitIssuesPage() {
                   _focus={{
                     boxShadow: "none",
                   }}
-                  onClick={() => setOpen(false)}
                 >
                   Closed Issues
                 </Tab>
               </TabList>
               <TabPanels>
                 <TabPanel height="500px" padding="15px 0px" overflow="scroll">
-                  {issues ? (
-                    <IssuesTable issues={issues} />
+                  {issues && issues.filter(findOpenIssues).length > 0 ? (
+                    <IssuesTable issues={issues.filter(findOpenIssues)} />
                   ) : (
-                    <Text>No Open Issues</Text>
+                    <Text alignSelf="center">No Open Issues</Text>
                   )}
                 </TabPanel>
                 <TabPanel height="500px" padding="15px 0px" overflow="scroll">
-                  {issues ? (
-                    <IssuesTable issues={issues} />
+                  {issues && issues.filter(findClosedIssues).length > 0 ? (
+                    <IssuesTable issues={issues.filter(findClosedIssues)} />
                   ) : (
                     <Text>No Closed Issues</Text>
                   )}
