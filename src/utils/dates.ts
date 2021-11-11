387e800d491a3e95dac228a2ad6dd683ae30275b
Saurav Ghosal <sghosal9@gatech.edu>
2021-11-11 16:45:33 -0500

    add date diff in utils

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -6,3 +6,12 @@ export function dateToMMDDYYYY(date: Date) {
 
   return `${month}/${day}/${year}`;
 }
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+export function dateDiffInDays(a: Date, b: Date) {
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
