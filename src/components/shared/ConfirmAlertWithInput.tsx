cd2773567cb354ca5c750efe5d88a7db16bb2061
mhartlage3 <madeline.hartlage@gatech.edu>
2022-02-23 21:46:00 -0500

    formatting

--- src/components/shared/ConfirmAlertWithInput.tsx
+++ src/components/shared/ConfirmAlertWithInput.tsx
@@ -1,52 +1,52 @@
 import {
-    Text,
-    Input,
-    Button,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    ModalCloseButton,
-  } from "@chakra-ui/react";
-  
-  interface Props {
-    title: string;
-    description: string;
-    confirmText: string;
-    onConfirm: () => void;
-    isOpen: boolean;
-    onClose: () => void;
-  }
-  
-  function ConfirmAlert(props: Props) {
-    const onConfirm = () => {
-      props.onConfirm();
-      props.onClose();
-    };
-  
-    return (
-      <Modal isOpen={props.isOpen} onClose={props.onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent w={{ base: "70%", md: "400px" }}>
-          <ModalHeader fontSize="2xl">{props.title}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Text fontSize="lg">{props.description}</Text>
-            <Input marginTop={4} placeholder="Reason for removal"></Input>
-          </ModalBody>
-          <ModalFooter>
-            <Button variant="danger" mr="10px" onClick={onConfirm}>
-              {props.confirmText}
-            </Button>
-            <Button variant="secondary" onClick={props.onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    );
-  }
-  
-  export default ConfirmAlert;
+  Text,
+  Input,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+interface Props {
+  title: string;
+  description: string;
+  confirmText: string;
+  onConfirm: () => void;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function ConfirmAlert(props: Props) {
+  const onConfirm = () => {
+    props.onConfirm();
+    props.onClose();
+  };
+
+  return (
+    <Modal isOpen={props.isOpen} onClose={props.onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent w={{ base: "70%", md: "400px" }}>
+        <ModalHeader fontSize="2xl">{props.title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text fontSize="lg">{props.description}</Text>
+          <Input marginTop={4} placeholder="Reason for removal" />
+        </ModalBody>
+        <ModalFooter>
+          <Button variant="danger" mr="10px" onClick={onConfirm}>
+            {props.confirmText}
+          </Button>
+          <Button variant="secondary" onClick={props.onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default ConfirmAlert;
