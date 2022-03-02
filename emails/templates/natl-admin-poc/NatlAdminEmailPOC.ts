9cc332b4dce730e17e8705c6f1966f32085c8aaf
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2022-03-02 14:06:43 -0500

    added natl admin email sending

---
+++ emails/templates/natl-admin-poc/NatlAdminEmailPOC.ts
@@ -0,0 +1,11 @@
+
+import { Email } from "src/utils/types";
+
+// StatusEmail is an example template
+export class NatlAdminEmailPOC implements Email {
+    readonly templateName = "natl-admin-poc";
+
+    constructor(
+        public readonly data: { name: string; content: string; subject: string; }
+    ) { }
+} 
--- src/pages/api/users/natlAdmin.ts
+++ src/pages/api/users/natlAdmin.ts
@@ -1,5 +1,7 @@
+import { NatlAdminEmailPOC } from "emails/templates/natl-admin-poc/NatlAdminEmailPOC";
 import { natlAdminGetChapterContacts } from "server/mongodb/actions/NatlAdmin";
 import APIWrapper from "server/utils/APIWrapper";
+import { sendEmail } from "server/utils/email";
 import { User, Role } from "src/utils/types";
 
 export default APIWrapper({
@@ -18,4 +20,36 @@ export default APIWrapper({
       return contacts;
     },
   },
+  POST: {
+    config: {
+      requireSession: true,
+      roles: [Role.NATIONAL_ADMIN],
+    }, handler: async (req) => {
+      const contacts: User[] = await natlAdminGetChapterContacts();
+      if (!contacts) {
+        throw new Error("Failed to retrieve contacts. Unable to send the email.");
+      }
+
+      const emails = contacts.map((contact) => contact.email);
+      const names = contacts.map((contact) => contact.name);
+      const emailsSent: string[] = [];
+
+      for (let index = 0; index < emails.length; index++) {
+        // Handles duplicate emails to avoid spam
+        if (emailsSent.includes(emails[index])) {
+          continue;
+        }
+
+        void sendEmail(
+          emails[index],
+          new NatlAdminEmailPOC({
+            name: names[index],
+            content: req.body.content as string,
+            subject: req.body.subject as string
+          }));
+
+        emailsSent.push(emails[index]);
+      }
+    }
+  }
 });
