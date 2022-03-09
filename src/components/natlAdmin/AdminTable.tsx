569025769d8a55a3f0199707c50c30f75d763ce5
mhartlage3 <madeline.hartlage@gatech.edu>
2022-03-09 13:38:47 -0500

    removed nonprofit segment

--- src/components/natlAdmin/AdminTable.tsx
+++ src/components/natlAdmin/AdminTable.tsx
@@ -91,164 +91,81 @@ function AdminTable({ chapters, nonprofits, chapterIsActive }: Props) {
           </Tr>
         </Thead>
         <Tbody overflowY="auto">
-          {chapters && chapterIsActive ? (
-            <>
-              {chapters.map((chapter) => (
-                <Tr
-                  key={chapter.name}
-                  height="100px"
-                  cursor="pointer"
-                  _hover={{ backgroundColor: "rgba(0, 105, 202, 0.05)" }}
-                >
-                  <Td
-                    paddingInlineStart={5}
-                    paddingInlineEnd={4}
-                    fontWeight={700}
-                  >
-                    {chapter.name}
-                  </Td>
-                  <Td paddingInlineStart={4} paddingInlineEnd={4}>
-                    <Link href={chapter.website} isExternal>
-                      {chapter.website}
-                    </Link>
-                  </Td>
-                  <Td paddingInlineStart={4} paddingInlineEnd={4}>
-                    <Link href={`mailto:${chapter.email}`} isExternal>
-                      {chapter.email}
-                    </Link>
-                  </Td>
-                  <Td paddingInlineStart={4} paddingInlineEnd={4}>
-                    5
-                  </Td>
-                  <Td>
-                    <Menu>
-                      <MenuButton>
-                        <Box as="button">
-                          <BsThreeDots />
-                        </Box>
-                      </MenuButton>
-                      <MenuList>
-                        <MenuItem
-                          borderBottomWidth={"thin"}
-                          color="#858585"
-                          fontWeight={"semibold"}
-                          paddingBottom={4}
-                          paddingLeft={4}
-                          fontFamily={"Tahoma"}
-                          fontStyle={"normal"}
-                          icon={<RiPencilFill size={20} />}
-                        >
-                          Edit Chapter
-                        </MenuItem>
-                        <Box
-                          as="button"
-                          width="100%"
-                          onClick={() =>
-                            handleClick(
-                              "Delete Chapter",
-                              "Please confirm that you would like to delete this chapter.",
-                              0
-                            )
-                          }
-                        >
-                          <MenuItem
-                            color="danger"
-                            fontWeight={"semibold"}
-                            paddingLeft={4}
-                            paddingTop={4}
-                            fontFamily={"Tahoma"}
-                            fontStyle={"normal"}
-                            icon={<FaTrash size={20} />}
-                          >
-                            Delete Chapter
-                          </MenuItem>
-                        </Box>
-                      </MenuList>
-                    </Menu>
-                  </Td>
-                </Tr>
-              ))}
-            </>
-          ) : (
-            <>
-              {nonprofits &&
-                nonprofits.map((nonprofit) => (
-                  <Tr
-                    key={nonprofit.name}
-                    height="100px"
-                    cursor="pointer"
-                    _hover={{ backgroundColor: "rgba(0, 105, 202, 0.05)" }}
-                  >
-                    <Td
-                      paddingInlineStart={5}
-                      paddingInlineEnd={4}
-                      fontWeight={600}
+          {chapters && chapters.map((chapter) => (
+            <Tr
+              key={chapter.name}
+              height="100px"
+              cursor="pointer"
+              _hover={{ backgroundColor: "rgba(0, 105, 202, 0.05)" }}
+            >
+              <Td
+                paddingInlineStart={5}
+                paddingInlineEnd={4}
+                fontWeight={700}
+              >
+                {chapter.name}
+              </Td>
+              <Td paddingInlineStart={4} paddingInlineEnd={4}>
+                <Link href={chapter.website} isExternal>
+                  {chapter.website}
+                </Link>
+              </Td>
+              <Td paddingInlineStart={4} paddingInlineEnd={4}>
+                <Link href={`mailto:${chapter.email}`} isExternal>
+                  {chapter.email}
+                </Link>
+              </Td>
+              <Td paddingInlineStart={4} paddingInlineEnd={4}>
+                5
+              </Td>
+              <Td>
+                <Menu>
+                  <MenuButton>
+                    <Box as="button">
+                      <BsThreeDots />
+                    </Box>
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem
+                      borderBottomWidth={"thin"}
+                      color="#858585"
+                      fontWeight={"semibold"}
+                      paddingBottom={4}
+                      paddingLeft={4}
+                      fontFamily={"Tahoma"}
+                      fontStyle={"normal"}
+                      icon={<RiPencilFill size={20} />}
                     >
-                      {nonprofit.name}
-                    </Td>
-                    <Td paddingInlineStart={4} paddingInlineEnd={4}>
-                      <Link href={nonprofit.website} isExternal>
-                        {nonprofit.website}
-                      </Link>
-                    </Td>
-                    <Td paddingInlineStart={4} paddingInlineEnd={4}>
-                      <Link
-                        href={`mailto:${(nonprofit.contact as User).email}`}
-                        isExternal
+                      Edit Chapter
+                    </MenuItem>
+                    <Box
+                      as="button"
+                      width="100%"
+                      onClick={() =>
+                        handleClick(
+                          "Delete Chapter",
+                          "Please confirm that you would like to delete this chapter.",
+                          0
+                        )
+                      }
+                    >
+                      <MenuItem
+                        color="danger"
+                        fontWeight={"semibold"}
+                        paddingLeft={4}
+                        paddingTop={4}
+                        fontFamily={"Tahoma"}
+                        fontStyle={"normal"}
+                        icon={<FaTrash size={20} />}
                       >
-                        {(nonprofit.contact as User).email}
-                      </Link>
-                    </Td>
-                    <Td>
-                      <Menu>
-                        <MenuButton>
-                          <Box as="button">
-                            <BsThreeDots />
-                          </Box>
-                        </MenuButton>
-                        <MenuList>
-                          <MenuItem
-                            borderBottomWidth={"thin"}
-                            color="#858585"
-                            fontWeight={"semibold"}
-                            paddingBottom={4}
-                            paddingLeft={4}
-                            fontFamily={"Tahoma"}
-                            fontStyle={"normal"}
-                            icon={<RiPencilFill size={20} />}
-                          >
-                            Edit Nonprofit
-                          </MenuItem>
-                          <Box
-                            as="button"
-                            width="100%"
-                            onClick={() =>
-                              handleClick(
-                                "Delete Nonprofit",
-                                "Please confirm that you would like to delete this nonprofit.",
-                                1
-                              )
-                            }
-                          >
-                            <MenuItem
-                              color="danger"
-                              fontWeight={"semibold"}
-                              paddingLeft={4}
-                              paddingTop={4}
-                              fontFamily={"Tahoma"}
-                              fontStyle={"normal"}
-                              icon={<FaTrash size={20} />}
-                            >
-                              Delete Nonprofit
-                            </MenuItem>
-                          </Box>
-                        </MenuList>
-                      </Menu>
-                    </Td>
-                  </Tr>
-                ))}
-            </>
-          )}
+                        Delete Chapter
+                      </MenuItem>
+                    </Box>
+                  </MenuList>
+                </Menu>
+              </Td>
+            </Tr>
+          ))}
         </Tbody>
       </Table>
     </>
